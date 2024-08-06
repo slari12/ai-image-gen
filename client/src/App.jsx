de2c11b3b6108a795b22ddb9fefc7697c072f174
clari <clari-v@gmail.com>
2024-08-06 21:51:32 +0800

    added router and dynamic components needed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import { Home, CreatePost } from "./pages";
 const App = () => {
   return (
     <BrowserRouter>
-      <header className="w-full flex justify-between items-center bg-white sm:px-8 py-4 border-b border-b-[#e6ebf4]">
+      <header className="w-full flex justify-between items-center bg-white sm:p-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to="/">
           <img src={logo} alt="logo" className="w-28 object-contain" />
         </Link>
