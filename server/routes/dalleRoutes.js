5b421e0d087a1fd1987422156ef45c9052d5237d
clari <clari-v@gmail.com>
2024-09-06 21:30:28 +0800

    changes added

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,7 +16,7 @@ router.route("/").get((req, res) => {
 router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
-    const aiResponse = await openai.Completion.create({
+    const aiResponse = await openai.images.generate({
       prompt,
       n: 1,
       size: "1024x1024",
