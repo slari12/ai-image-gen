1467cf4b92738e69ec347593da3fa3be455e39e1
slari12 <blue.atlantic09@gmail.com>
2024-09-11 17:55:22 +0800

    fix ai response

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -13,20 +13,23 @@ const openai = new OpenAI({
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello from DALL-E!" });
 });
+
 router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
+
     const aiResponse = await openai.images.generate({
+      model: "dall-e-3",
       prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
     });
-    const image = aiResponse.data.choices[0].image;
-    res.status(200).json({ photo: image });
+    const img_url = aiResponse.data[0].b64_json;
+    res.status(200).json({ photo: img_url });
   } catch (error) {
     console.log(error);
-    res.status(500).send(error);
+    res.status(500).send(error.message || "Internal Server Error");
   }
 });
 
